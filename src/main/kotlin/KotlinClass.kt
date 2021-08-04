b41d8f5ec7f459f4f47256624ec51845f268d86f
MdAbdullahAlMahmud <mmunna864@gmail.com>
2021-08-04 23:27:45 +0600

    Kotlin class & Constructor , inheritance

---
+++ src/main/kotlin/KotlinClass.kt
@@ -0,0 +1,61 @@
+ open class  KotlinClass {
+
+    var a: Int? = null
+    init {
+        a = 40
+    }
+     constructor( name: String)  {
+
+     }
+    constructor(name :String , age : Int):this(name){
+        println("Second constructor is called")
+    }
+
+
+     /**
+      * if we want to make this class to be parent class for another one then
+      * in order to inherit any fun , we have to make it open cause by default all class & fun are final
+      */
+
+     open fun display() {
+        println("Value of a is $a")
+
+    }
+}
+
+fun main() {
+
+    var my_class = KotlinClass("Munna")
+    my_class.display()
+
+    val derievedClass = ChildClass("Munna",18,"Naogaon")
+    derievedClass.display()
+}
+
+class ChildClass : KotlinClass {
+
+    var name :String? = null
+    var address :String? = null
+    var age :Int? = null
+
+
+    /**
+     * constructor
+     * we are initializing name & age properties by the parent class's properties
+     */
+    constructor(name :String , age : Int , address :String ): super(name,age){
+       this.name= name
+       this.age = age
+       this.address = address
+    }
+
+
+    /**
+     * override properties
+     */
+    override fun display() {
+        super.display()
+        println("Address $address")
+    }
+}
+
